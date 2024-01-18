27cf60b74287bd51140f29e0677f22569392d9f4
JuliethGiraldo <juliethgs0101@gmail.com>
2024-01-17 21:10:09 -0500

    Actualizacion

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+let numeroSecreto = 4;
 let eleccion = prompt('Elige un número entre 1 y 10');
 
-let numeroSecreto = 4;
+console.log(numeroUsuario);
 
 if (eleccion == numeroSecreto) {
     alert('Adivinaste');
